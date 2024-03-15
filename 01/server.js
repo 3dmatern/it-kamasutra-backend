d06bce11393866cab4a687c031405ecf040801b9
Denis Matern <denismatern@gmail.com>
2024-03-15 20:15:10 +0300

    done: homework video 05

--- 01/server.js
+++ 01/server.js
@@ -1,20 +1,32 @@
+// Ответ на ДЗ https://code.mu/ru/javascript/nodejs/book/prime/server/resources/
+
 const http = require("http");
+const fs = require("fs");
 
 let requestsCount = 0;
 
-const server = http.createServer((request, response) => {
-    requestsCount++;
+const server = http.createServer(async (request, response) => {
+    if (request.url != "/favicon.ico") {
+        requestsCount++;
 
-    switch (request.url) {
-        case "/students":
-            response.write("STUDENTS");
-            break;
-        case "/":
-        case "/courses":
-            response.write("FRONT + BACK");
-            break;
-        default:
-            response.write("404 Not Found");
+        switch (request.url) {
+            case "/students":
+                response.write("STUDENTS");
+                break;
+            case "/":
+            case "/courses":
+                response.write("FRONT + BACK");
+                break;
+            default:
+                response.write("404 Not Found");
+                response.statusCode = 404;
+        }
+
+        response.write(" IT-KAMASUTRA: " + requestsCount);
+    } else {
+        let favicon = await fs.promises.readFile("favicon.ico");
+        response.writeHead(200, { "Content-Type": "image/x-icon" });
+        response.write(favicon);
     }
 
     response.end();
